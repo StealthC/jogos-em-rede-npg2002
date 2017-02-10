a9e05951ba3ba5d6cce66e2b1b2f0a385e97ce2e
Carlos Alberto Castelo Elias Filho <cacefmail@gmail.com>
2017-02-10 10:20:59 -0300

    Fim Aula 06

--- Assets/Scripts/Tank.cs
+++ Assets/Scripts/Tank.cs
@@ -5,6 +5,8 @@ using UnityEngine.Networking;
 
 public class Tank : NetworkBehaviour {
 
+    public Rigidbody2D bulletPrefab;
+
     public override void OnStartLocalPlayer()
     {
         GetComponent<SpriteRenderer>().color = Color.red;
@@ -20,5 +22,16 @@ public class Tank : NetworkBehaviour {
 
         transform.Rotate(0, 0, -h * 180 * Time.deltaTime);
         transform.Translate(0, v * 4 * Time.deltaTime, 0f);
+
+        if (Input.GetKeyDown(KeyCode.Z)) CmdFire();
+    }
+
+    [Command]
+    void CmdFire()
+    {
+        Rigidbody2D bullet = (Rigidbody2D)Instantiate(bulletPrefab, transform.position + transform.up, transform.rotation);
+        bullet.velocity = transform.up * 6;
+        NetworkServer.Spawn(bullet.gameObject);
+        Destroy(bullet.gameObject, 1f);
     }
 }
